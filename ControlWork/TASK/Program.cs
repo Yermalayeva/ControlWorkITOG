2401080099273b3ba822fcf1880d21c7d8077b8c
Natalya Yermolaeva <natali.ermol73@mail.ru>
2023-01-29 04:02:44 +0300

    init:add new changes to ControlWork

---
+++ ControlWork/TASK/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача: Написать програмуу, которая из имеющегося массива строк 
+// сформирует массив из строк, длина которого меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма.
+
+string[] arrayFirst = new string[7] {"work", ":-)", "monday", "54", "friday", "(", "dog"};
+string[] arraySecond = new string[arrayFirst.Length];
+
+void SecondArrayFromFirst(string[] arrayFirst, string[] arraySecond)
+{
+    int count = 0;
+    for(int i = 0; i < arrayFirst.Length; i ++)
+    {
+        if(arrayFirst[i].Length <= 3)
+        {
+            arraySecond[count] = arrayFirst[i];
+            count++;
+        }
+    }
+    
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayFromFirst(arrayFirst, arraySecond);
+PrintArray(arraySecond);
+
